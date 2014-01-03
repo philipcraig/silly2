8d9dfe0c673f38b641e8bf4a1bf1c17a9c32643e
Philip Craig <philip@pobox.com>
2014-01-03 14:50:00 +0000

    Initial commit

--- cyclopeptide_sequencing.py
+++ cyclopeptide_sequencing.py
@@ -1,12 +1,31 @@
 import genome
 
-weights = genome.readUniqueWeights()
+
+def expandCandidates(candidates, weights):
+    result = []
+    for weight in weights:
+        for candidate in candidates:
+            newCandidate=[]
+            newCandidate.extend(candidate)
+            newCandidate.extend([weight])
+            result.append(newCandidate)
+    return result
+
+weights = set(genome.readUniqueWeights())
 
 spectrum = [0,113,128,186,241,299,314,427]
 
-list = [[0]]
-#while len(result) != 0:
-
-expandList(list)
-for peptide in list:
-    if
+candidates = [[0]]
+while len(candidates) > 0:
+    candidates = expandCandidates(candidates, weights)
+    # for peptide in list:
+    i = 0
+    while i < len(candidates):
+        peptide = candidates[i]
+        if sum(peptide) == spectrum[-1]:
+            print peptide[1:]
+            candidates.remove(peptide)
+        elif sum(peptide) not in spectrum:
+            candidates.remove(peptide)
+        else:
+            i += 1

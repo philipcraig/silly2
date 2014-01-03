ff085c55b68388d42b744812c184b4acd840144c
Philip Craig <philip@pobox.com>
2014-01-03 19:54:13 +0000

    Get extra data set, check subsequence validity, make fast

--- cyclopeptide_sequencing.py
+++ cyclopeptide_sequencing.py
@@ -2,30 +2,44 @@ import genome
 
 
 def expandCandidates(candidates, weights):
-    result = []
+    result = set()
     for weight in weights:
         for candidate in candidates:
-            newCandidate=[]
-            newCandidate.extend(candidate)
-            newCandidate.extend([weight])
-            result.append(newCandidate)
+            newCandidate = candidate + (weight,)
+            result.add(newCandidate)
     return result
+
+
+def sublists(peptide):
+    for j in range(len(peptide)):
+        for i in range(len(peptide) - j + 1):
+            yield peptide[i:i + j]
+
+def consistent(peptide, spectrum):
+    for candidate in sublists(peptide):
+        if sum(candidate) not in spectrum:
+            return False
+    return True
 
 weights = set(genome.readUniqueWeights())
 
-spectrum = [0,113,128,186,241,299,314,427]
+spectrum = [0, 113, 128, 186, 241, 299, 314, 427]
 
-candidates = [[0]]
-while len(candidates) > 0:
+inpFile = open('C:/temp/cycloseq_data.txt', 'r')
+inpFile.readline()
+spectrum = map(int, inpFile.readline().split())
+
+zero_tuple=0,
+candidates = set()
+candidates.add(zero_tuple)
+while candidates:
     candidates = expandCandidates(candidates, weights)
-    # for peptide in list:
-    i = 0
-    while i < len(candidates):
-        peptide = candidates[i]
+    newCandidates = set()
+    for peptide in candidates:
         if sum(peptide) == spectrum[-1]:
             print peptide[1:]
-            candidates.remove(peptide)
-        elif sum(peptide) not in spectrum:
-            candidates.remove(peptide)
+        elif not consistent(peptide, spectrum):
+            None # do nothing, so it isn't in newCandidates
         else:
-            i += 1
+            newCandidates.add(peptide)
+    candidates = newCandidates
